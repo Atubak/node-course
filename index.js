45651dd170e2323aad92237b8a01bb3fc50b2d1b
Jonatan Kabuta <jonatankabuta@Jonatans-MacBook-Pro.local>
2023-08-04 14:06:50 +0200

    #30 done

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+process.env.UV_THREADPOOL_SIZE = 1; // default 4
 const cluster = require("cluster");
+const crypto = require("crypto");
 const express = require("express");
 
 console.log({
@@ -7,26 +9,20 @@ console.log({
 
 const app = express();
 
-function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {
-        // do nothing
-    }
-}
-
 if (cluster.isMaster) {
     // Cause index.js to be executed *again* but in child mode
     // a single call creates a single child, same as calling index.js without cluster
     // to create multiple children, call fork multiple times
 
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 1; i++) {
         cluster.fork();
     }
 } else {
     // Im a child, Im going to act like a server and do nothing else
     app.get("/", (req, res) => {
-        doWork(5000);
-        res.send("Hi there");
+        crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+            res.send("Hi there");
+        });
     });
 
     app.get("/fast", (req, res) => {
